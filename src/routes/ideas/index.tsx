14cf3f817599308e7f6fbf5b6e6270217dfb73ca
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-05-09 17:58:16 +0300

    Added a new title meta tag to the head of the Ideas page

--- src/routes/ideas/index.tsx
+++ src/routes/ideas/index.tsx
@@ -1,6 +1,13 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/ideas/")({
+  head: () => ({
+    meta: [
+      {
+        title: "IdeaHub - Browse Ideas",
+      },
+    ],
+  }),
   component: IdeasPage,
 });
 
