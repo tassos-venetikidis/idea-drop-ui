c298757cd06c68e5838eb423129328e3dbfcba58
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-05-14 18:16:49 +0300

    Created the IdeaCard component that includes the conditional display of the details linkas a button or as a simple link

---
+++ src/components/IdeaCard.tsx
@@ -0,0 +1,27 @@
+import type { Idea } from "#/types";
+import { Link } from "@tanstack/react-router";
+import clsx from "clsx";
+
+function IdeaCard({ idea, button = true }: { idea: Idea; button?: boolean }) {
+  const linkClasses = clsx({
+    "w-full text-center mt-4 inline-block bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition":
+      button,
+    "text-blue-600 hover:underline mt-3": !button,
+  });
+
+  return (
+    <div className="border border-gray-300 rounded-lg shadow p-4 bg-white">
+      <h3 className="text-lg font-bold text-gray-900">{idea.title}</h3>
+      <p className="text-gray-600 mb-2">{idea.summary}</p>
+      <Link
+        to="/ideas/$ideaId"
+        params={{ ideaId: idea.id.toString() }}
+        className={linkClasses}
+      >
+        {button ? "View Idea" : "Read more →"}
+      </Link>
+    </div>
+  );
+}
+
+export default IdeaCard;
