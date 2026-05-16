02ee0300488ad77e23e412e3fe827a893535ab54
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-05-16 15:48:40 +0300

    Implemented the createIdea async function

--- src/api/ideas.ts
+++ src/api/ideas.ts
@@ -10,3 +10,17 @@ export async function fetchIdeas(): Promise<Idea[]> {
   const res = await api.get("/ideas");
   return res.data;
 }
+
+export async function createIdea(newIdea: {
+  title: string;
+  summary: string;
+  description: string;
+  tags: string[];
+}): Promise<Idea> {
+  const res = await api.post("/ideas", {
+    ...newIdea,
+    createdAt: new Date().toISOString(),
+  });
+
+  return res.data;
+}
