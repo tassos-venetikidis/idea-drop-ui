b0c0acdd09c36e8568601e76bab6604eac29f167
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-05-20 22:38:55 +0300

    Made minor modifications that pertain to the fact that sorting and limiting of ideas delivered is done on the back-end

--- src/routes/ideas/index.tsx
+++ src/routes/ideas/index.tsx
@@ -23,10 +23,7 @@ export const Route = createFileRoute("/ideas/")({
 });
 
 function IdeasPage() {
-  const { data } = useSuspenseQuery(ideasQueryOptions());
-  const ideas = [...data].sort(
-    (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
-  );
+  const { data: ideas } = useSuspenseQuery(ideasQueryOptions());
 
   return (
     <div className="p-4">
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,8 +6,8 @@ import IdeaCard from "#/components/IdeaCard";
 
 const ideasQueryOptions = () =>
   queryOptions({
-    queryKey: ["ideas"],
-    queryFn: () => fetchIdeas(),
+    queryKey: ["ideas", { limit: 3 }],
+    queryFn: () => fetchIdeas(3),
   });
 
 export const Route = createFileRoute("/")({
@@ -17,10 +17,7 @@ export const Route = createFileRoute("/")({
 });
 
 function HomePage() {
-  const { data } = useSuspenseQuery(ideasQueryOptions());
-  const ideas = [...data].sort(
-    (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
-  );
+  const { data: ideas } = useSuspenseQuery(ideasQueryOptions());
 
   return (
     <div className="flex flex-col md:flex-row items-start justify-between gap-10 p-6 text-blue-600">
