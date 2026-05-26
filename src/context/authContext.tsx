4199b70ea858f183714558b342ad1e580206a24f
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-05-26 16:42:20 +0300

    Modified the token parameter of the setAccessToken to be a union type of string and null

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -2,7 +2,7 @@ import { useState, createContext, useContext, type ReactNode } from "react";
 
 type AuthContextType = {
   accessToken: string | null;
-  setAccessToken: (token: string) => void;
+  setAccessToken: (token: string | null) => void;
   user: {
     id: string;
     name: string;
