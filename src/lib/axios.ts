8850597b8f57d088f4d2a2704a4c28dc1fe4e1fa
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-05-28 18:42:01 +0300

    Implemented the interceptor that refreshes the access token when it expires

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { getStoredAccessToken } from "./authToken.ts";
+import { getStoredAccessToken, setStoredAccessToken } from "./authToken.ts";
+import { refreshAccessToken } from "#/api/auth.tsx";
 
 const base =
   typeof window !== "undefined"
@@ -18,5 +19,28 @@ api.interceptors.request.use((config) => {
   config.headers.Authorization = `Bearer ${getStoredAccessToken()}`;
   return config;
 });
+
+api.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    const originalRequest = error.config;
+    if (
+      error.response?.status === 401 &&
+      !originalRequest._retry &&
+      !originalRequest.url.includes("/auth/refresh")
+    ) {
+      originalRequest._retry = true;
+      try {
+        const { accessToken: newToken } = await refreshAccessToken();
+        setStoredAccessToken(newToken);
+        originalRequest.headers.Authorization = `Bearer ${newToken}`;
+        return api(originalRequest);
+      } catch (error) {
+        console.error("Refresh token failed");
+      }
+    }
+    return Promise.reject(error);
+  },
+);
 
 export default api;
